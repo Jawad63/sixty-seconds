f396cde747646461813667838da8094caa12646c
Jawad63 <jawadqureshi111@gmail.com>
2021-10-13 10:51:52 +0200

    material ui installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Sixty Seconds</h1>
+
+      {/* List of Components to make: */}
+      
+      {/* Header */}
+      <Header />
+      {/* Cards */}
+      {/* Buttons at the bottom */}
+      {/* Chat screen */}
+
+
     </div>
   );
 }
