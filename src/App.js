92016b5c8ac2c64585fb432f3ff5da8571452355
Jawad63 <jawadqureshi111@gmail.com>
2021-10-14 17:40:02 +0200

    still working on styling for main page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import './App.css';
+import React from 'react';
 import Header from './components/Header/Header.js';
-
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
 
-      {/* List of Components to make: */}
-      
       {/* Header */}
       <Header />
-      {/* Cards */}
-      {/* Buttons at the bottom */}
-      {/* Chat screen */}
+
+      <Router>
+        <Switch>
+
+          <Route path="/chat">
+            <h1>This is chatpage</h1>
+          </Route>
+
+          <Route path="/">
+            <h1>This is homepage</h1>
+          </Route>
+
+        </Switch>
+      </Router>
+
+
+
+        {/* Cards */}
+
+        {/* Buttons at the bottom */}
+
+        {/* Chat screen */}
 
 
     </div>
