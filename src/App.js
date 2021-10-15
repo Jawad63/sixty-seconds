ff9a86c819782b3c1e5f0577c5d32a4507097f31
Jawad63 <jawadqureshi111@gmail.com>
2021-10-15 08:35:28 +0200

    creating componnets files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header.js';
+import Cards from './components/Cards/cards.js';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -7,6 +8,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+
 
 function App() {
   return (
@@ -18,8 +20,17 @@ function App() {
       <Router>
         <Switch>
 
+          <Route path="/profile">
+            <h1>This is profile page</h1>
+          </Route>
+
           <Route path="/chat">
             <h1>This is chatpage</h1>
+          </Route>
+
+          {/* Cards */}
+          <Route path="/cards">
+            <Cards />
           </Route>
 
           <Route path="/">
@@ -31,7 +42,7 @@ function App() {
 
 
 
-        {/* Cards */}
+
 
         {/* Buttons at the bottom */}
 
---
+++ src/components/Cards/cards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../Cards/cards.css"
+function cards() {
+   return (
+      <div>
+         <h1>This is card section</h1>
+      </div>
+   )
+}
+
+export default cards
